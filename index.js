b5befcbdc55c4c6c90f9e2836ff62f4aed97d822
josue rojas <josuerojas.rojas@gmail.com>
2017-09-27 18:32:02 -0400

    added submit form for new event and call

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// constants
+monthToIndex={
+  'January': 0,
+  'February': 1,
+  'March': 2,
+  'April': 3,
+  'May': 4,
+  'June': 5,
+  'July': 6,
+  'August': 7,
+  'September': 8,
+  'October': 9,
+  'November': 10,
+  'December':11
+}
 // ----------------------- node stuff --------------------
 var express = require('express');
 var pg = require('pg'); //postgres
@@ -11,6 +26,8 @@ app.set('view engine', 'ejs')
 var connectionString = process.env.DATABASE_URL;
 var pgClient = new pg.Client(connectionString);
 pgClient.connect();
+// create table if none
+pgClient.query("CREATE TABLE IF NOT EXISTS events(id SERIAL UNIQUE PRIMARY KEY, year INTEGER  NOT NULL, monthNum INTEGER NOT NULL, month CHAR(255) NOT NULL, day INTEGER NOT NULL, hourStart INTEGER  NOT NULL, minStart INTEGER NOT NULL, hourEnd INTEGER NOT NULL, minEnd INTEGER NOT NULL, priority INTEGER NOT NULL, description text NOT NULL)")
 
 // ----------------------- functions --------------------
 // note: months start at 0 so jan -> 0
@@ -25,12 +42,13 @@ function FirsDayNameOfMonth(Year, Month) {
 function numToMonth(Month){
   return (['January','February', 'March', 'April', 'May', 'June', 'July', 'August','September','October','November','December'])[Month]
 }
+
 // ----------------------- routes -----------------------
 // current month and default route
 app.get('/', (req, res, next) => {
   thisMonth = (new Date()).getMonth()
   thisYear = (new Date()).getFullYear()
-  res.render('calender', {date_end:LastDayOfMonth(thisYear, thisMonth), date_name_start:FirsDayNameOfMonth(thisYear, thisMonth), thisMonth:numToMonth(thisMonth)});
+  res.render('calender', {date_end:LastDayOfMonth(thisYear, thisMonth), date_name_start:FirsDayNameOfMonth(thisYear, thisMonth), thisMonth:numToMonth(thisMonth), year:thisYear});
 })
 
 // current month if no query (month and year)
@@ -38,14 +56,17 @@ app.get('/', (req, res, next) => {
 app.get('/date', (req, res) => {
   thisMonth =  !req.query.month ? (new Date()).getMonth() : req.query.month
   thisYear = !req.query.year ? (new Date()).getFullYear() : req.query.year
-  res.render('calender', {date_end:LastDayOfMonth(thisYear, thisMonth), date_name_start:FirsDayNameOfMonth(thisYear, thisMonth), thisMonth:numToMonth(thisMonth)});
+  res.render('calender', {date_end:LastDayOfMonth(thisYear, thisMonth), date_name_start:FirsDayNameOfMonth(thisYear, thisMonth), thisMonth:numToMonth(thisMonth), year:thisYear});
 })
-
 
 app.get('/*', (req, res) =>{
   res.redirect('/');
 })
 
+app.post('/event',(req, res) => {
+  pgClient.query('INSERT INTO events(id, year, monthNum, month, day, hourStart, minStart, hourEnd, minEnd, priority, description) values(DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',[req.body.year, req.body.monthNum, req.body.month, req.body.day, req.body.hourStart, req.body.minStart, req.body.hourEnd, req.body.minEnd, req.body.priority, req.body.description])
+  res.end('{success : "Updated Successfully", "status" : 200}');
+})
 
 // ----------------------- start -----------------------
 app.listen(process.env.PORT);
--- public/main.js
+++ public/main.js
@@ -1,6 +1,81 @@
-function LastDayOfMonth(Year, Month) {
-  var t= new Date(Year, Month-1);
-  console.log(t)
-  return (new Date(t.getFullYear(), t.getMonth() + 1, 0, 23, 59, 59));
+// --------------------- constants & vars--------------------------
+monthToIndex={
+  'January': 0,
+  'February': 1,
+  'March': 2,
+  'April': 3,
+  'May': 4,
+  'June': 5,
+  'July': 6,
+  'August': 7,
+  'September': 8,
+  'October': 9,
+  'November': 10,
+  'December':11
 }
-console.log(LastDayOfMonth(2017, 9));
+var year, month, day;
+var priority = 0 // 0 -> low, 1 -> medium, 2 -> high
+
+// --------------------- dom ready stuff --------------------------
+// add on click to those with numbers and not the empty ones, and set constants
+$( document ).ready(function() {
+  $body = $('.body')
+  year = $body.data('year')
+  month = $body.data('month')
+  var days = $('.date');
+  for(var i = 0; i < days.length; i++){
+    if(!($(days[i]).find('.day-number').length == 0)){
+      $(days[i]).click(function(){eventsStuff(this)})
+    }
+    else{
+      $(days[i]).addClass('no-date')
+    }
+  }
+  $('.btn.priority').click(function(){disableButton(this)})
+});
+
+// --------------------- funtions (clicks and such) --------------------------
+//this will set the priority
+function disableButton(target){
+  $('.btn.priority').removeClass('disabled')
+  $(target).addClass('disabled')
+  priority = $(target).data('priority')
+}
+
+// to show events and forms
+function eventsStuff(target){
+  day = $(target).find('.day-number').data('day')
+  $events = $('.events')
+  $events.find('.events-date').text(month + ' ' + day + ', ' + year)
+  $events.addClass('show')
+  // console.log(new Date(year, monthToIndex[month], day))
+}
+
+// --------------------- api requests --------------------------
+// api calls
+function submitNew(){
+   $("#hour-start").find(':selected').text()
+  newData = {
+    'year': year,
+    'monthNum': monthToIndex[month],
+    'month': month,
+    'day': day,
+    'hourStart':parseInt($("#hour-start").find(':selected').text()),
+    'minStart': parseInt($("#min-start").find(':selected').text()),
+    'hourEnd':parseInt($("#hour-end").find(':selected').text()),
+    'minEnd': parseInt($("#min-end").find(':selected').text()),
+    'description': $('#description').val(),
+    'priority': parseInt(priority)
+  }
+  $.ajax({
+    type:'POST',
+    url:'/event',
+    datatype:'json',
+    contentType: 'application/json',
+    data: JSON.stringify(newData),
+    success:function(res){
+      window.location = window.location; //refresh
+    }
+  })
+  console.log(newData)
+}
